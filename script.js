548b85108c362438f417ad6647190d84ac998a7e
rafssjesus <148909191+rafssjesus@users.noreply.github.com>
2025-11-20 14:47:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
-}
+console.log("Site carregado com sucesso!");
